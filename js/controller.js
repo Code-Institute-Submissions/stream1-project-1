64cb0eb26be0ac69724904b25eda140a692b9e25
Maisie Campbell <maisie-campbell@hotmail.com>
2017-03-05 21:03:21 +0000

    added modal fucntionality to MediaController

--- js/controller.js
+++ js/controller.js
@@ -8,6 +8,23 @@ angular.module('RouteControllers', [])
 	})
 	.controller('MediaController', function($scope){
 
+		$(document).ready(function(){
+			// preload images
+			$('.modal-thumb').each( function () {
+			    img = new Image();
+			    img.src = $(this).data('modal-src');
+			});
+
+			$('.modal-thumb').on('click', function () {
+			    var title = $(this).data('modal-title'),
+			        src = $(this).data('modal-src');
+			    
+			    $('#modal h4').text(title);
+			    $('#modal img').attr('src', src);
+
+			    $('#modal').modal('show');
+			});				
+	});
 	})
 	.controller('GigController', function($scope){
 
