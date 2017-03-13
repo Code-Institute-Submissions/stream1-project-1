356d99235044c18e27b05450e75cfadfa1928e5c
Maisie Campbell <maisie-campbell@hotmail.com>
2017-03-13 13:58:22 +0000

    created directive for the footer

---
+++ js/directive.js
@@ -0,0 +1,6 @@
+angular.module('FooterDirective', []).directive('footerDirective', function() {
+	return {
+		restrict: 'EA', // E/A -> element/attribute
+		templateUrl: '/templates/directives/footerDirective.html'
+	};
+});
